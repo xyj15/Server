194100d6991ee25fec2b3856c0a90fa21e5a2176
xyj15 <591024170@qq.com>
2017-01-01 16:02:50 +0800

    ui

---
+++ src/main/java/ui/Controller.java
@@ -0,0 +1,26 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import rmi.RemoteHelper;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * Created by 婧婧 on 2017/1/1.
+ */
+public class Controller {
+    private static Parent root;
+    private static Stage primaryStage;
+
+    public static void setRoot(Parent root) {
+        Controller.root = root;
+    }
+
+    public static void setPrimaryStage(Stage primaryStage) {
+        Controller.primaryStage = primaryStage;
+    }
+
+
+}
